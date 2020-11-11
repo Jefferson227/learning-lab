bf2232e8d57e3edcdd1f4c92e34ad832585a9329
Jefferson Matheus <jefferson227@gmail.com>
2020-11-11 16:24:50 -0300

    Add form subtitle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <section className="App-container">
       <div className="App-container__form">
         <h1 className="App-container__form-title">Create account</h1>
+        <h5 className="App-container__form-subtitle">Already have an account? <a href="http://localhost:8080">Sign in</a></h5>
       </div>
     </section>
   );
