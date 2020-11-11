23e0b2bf16118d52639bf06f5cf572eeaa680f43
Jefferson Matheus <jefferson227@gmail.com>
2020-11-11 17:24:25 -0300

    Add fullsized text inputs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
       <div className="App-container__form">
         <h1 className="App-container__form-title">Create account</h1>
         <h5 className="App-container__form-subtitle">Already have an account? <a href="http://localhost:8080">Sign in</a></h5>
+
+        <form className="form-group">
+          <input type="text" className="form-group__input-text form-group__input-text--fullsize" placeholder="Name" />
+          <input type="email" className="form-group__input-text form-group__input-text--fullsize" placeholder="Email" />
+        </form>
       </div>
     </section>
   );
