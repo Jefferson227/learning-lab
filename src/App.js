bc97cc1027a1ba646ba8620bcad89e6929aab8e1
Jefferson Matheus <jefferson227@gmail.com>
2020-11-11 17:43:42 -0300

    Add first name and last name inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.scss';
 
+// https://dribbble.com/shots/14019613-Sign-up-form
+
 function App() {
   return (
     <section className="App-container">
@@ -9,6 +11,8 @@ function App() {
 
         <form className="form-group">
           <input type="text" className="form-group__input-text form-group__input-text--fullsize" placeholder="Name" />
+          <input type="text" className="form-group__input-text form-group__input-text--halfsize-left" placeholder="First name" />
+          <input type="text" className="form-group__input-text form-group__input-text--halfsize-right" placeholder="Last name" />
           <input type="email" className="form-group__input-text form-group__input-text--fullsize" placeholder="Email" />
         </form>
       </div>
